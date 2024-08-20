3da1d77005fdd0a5451eaee559106340d0b0f6f9
Youssef Emira <138404003+joeemira@users.noreply.github.com>
2024-08-20 12:48:34 +0300

    Add files via upload

---
+++ output the mails from file/script.py
@@ -0,0 +1,14 @@
+emails = []
+pattern = re.compile(r"[^@]+@[^@]+\.[a-zA-Z]{2,}")
+
+with open("C:/Users/youss/OneDrive/Desktop/python/file.txt", "r") as file:
+    for line in file:
+        line = line.strip()
+        if pattern.match(line) and line not in emails:
+            emails.append(line)
+
+with open(r"C:/Users/youss/OneDrive/Desktop/python/emails.txt", 'w') as output:
+    for email in emails:
+        output.write(email + '\n')
+
+output_file = 'C:/Users/youss/OneDrive/Desktop/python/emails.txt'
